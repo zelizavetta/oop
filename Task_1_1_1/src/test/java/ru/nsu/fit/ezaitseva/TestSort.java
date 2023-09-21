6149ad059e8d4e0db9d7279774c1880d4c6bc97c
z.elizavetta <e.zaitseva2@g.nsu.ru>
2023-09-21 18:25:04 +0700

    исправлены замечания исправлены тесты

--- Task_1_1_1/src/test/java/ru/nsu/fit/ezaitseva/TestSort.java
+++ Task_1_1_1/src/test/java/ru/nsu/fit/ezaitseva/TestSort.java
@@ -1,29 +1,31 @@
 package ru.nsu.fit.ezaitseva;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class TestSort {
-    //    private static Stream<int[]> numbers() {
-//        int[][] a = {{1,2},{3,4}, {-5,5}};
-//        return Stream.of(
-//                a
-//        );
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("numbers")
-//    void testSum(int[] ar){
-//        Assertions.assertEquals(SomeClass.sum(ar[0],ar[1]), ar[0]+ar[1]);
-//    }
-    @Test
-    public void testSort() {
-        int[] arr = {5, 4, 1, 2, 10, -5};
-        int[] sorted = arr.clone();
-        Arrays.sort(sorted);
+
+    @ParameterizedTest
+    @MethodSource("checkHeapSort")
+    void testSort(int[] arr, int[] sortedArr) {
         Heapsort.sort(arr);
-        Assertions.assertArrayEquals(arr, sorted);
+        Assertions.assertArrayEquals(arr, sortedArr);
     }
+
+    private static Stream<Arguments> checkHeapSort() {
+        return Stream.of(
+                Arguments.of(new int[]{0, 1, 4, 2, 3}, new int[]{0, 1, 2, 3, 4}),
+                Arguments.of(new int[]{5, 6, 9, 23}, new int[]{5, 6, 9, 23}),
+                Arguments.of(new int[]{10, 10, 10, 10, 10}, new int[]{10, 10, 10, 10, 10}),
+                Arguments.of(new int[]{-4, -2, -76, -2}, new int[]{-76, -4, -2, -2}),
+                Arguments.of(new int[]{-4, 56, -76, 23}, new int[]{-76, -4, 23, 56}),
+                Arguments.of(new int[]{5}, new int[]{5}),
+                Arguments.of(new int[]{}, new int[]{})
+        );
+    }
+
 }
