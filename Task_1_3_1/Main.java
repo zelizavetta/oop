529b5b527daa1821ee2baeda7e4c2983b15f5606
zelizavetta <e.zaitseva2@g.nsu.ru>
2023-10-22 13:32:34 +0700

    Task_1_3_1 version 1 without tests and javadocs

---
+++ Task_1_3_1/Main.java
@@ -0,0 +1,7 @@
+package Task_1_3_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Task_1_3_1/src/main/java/ru/nsu/fit/ezaitseva/SubStringSearch.java
@@ -0,0 +1,49 @@
+package ru.nsu.fit.ezaitseva;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubStringSearch {
+    public static String root = "C:/Users/Elisa/oop/Task_1_3_1/src/main/java/ru/nsu/fit/ezaitseva/";
+
+    public static List<Integer> search(String p, String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        List<Integer> result = new ArrayList<>();
+        int[] prefixFunc = new int[p.length()];
+        Arrays.fill(prefixFunc, 0);
+        int prefixFuncPrev = 0;
+        int quantSymb = 0;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            String perfixLine = p + '#' + line;
+            for (int i = 1; i < perfixLine.length(); i++) {
+                int k = prefixFuncPrev;
+                while (k > 0 && perfixLine.charAt(k) != perfixLine.charAt(i)) {
+                    k = prefixFunc[k - 1];
+                }
+                if (perfixLine.charAt(k) == perfixLine.charAt(i)) {
+                    k++;
+                }
+                if (i < p.length()) {
+                    prefixFunc[i] = k;
+                }
+                prefixFuncPrev = k;
+                if (k == p.length()) {
+                    result.add(i + quantSymb - 2 * p.length());
+                }
+            }
+            quantSymb += line.length();
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String searchingStr = "bra";
+        List<Integer> result = search(searchingStr, root + "input.txt");
+        System.out.println(result);
+    }
+}
