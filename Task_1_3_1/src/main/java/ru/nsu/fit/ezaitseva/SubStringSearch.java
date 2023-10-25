b48955b0f946db1a329c33f0dbffa1851889ddae
zelizavetta <e.zaitseva2@g.nsu.ru>
2023-10-25 19:33:28 +0700

    Task_1_3_1 version 3

--- Task_1_3_1/src/main/java/ru/nsu/fit/ezaitseva/SubStringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/fit/ezaitseva/SubStringSearch.java
@@ -6,19 +6,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SubStringSearch {
+    /**
+     * root - fixed part of the path
+     */
     public static String root = "C:/Users/Elisa/oop/Task_1_3_1/";
 
+    /**
+     * function search is used for searching string in file using prefix-function.
+     * it calculates prefix-function from prefix-line (p#line).
+     * @param p - string that we are searching for
+     * @param filename - name of the input file
+     * @return array of Integers - array of occurrence indexes of string p
+     * @throws IOException when file not found or some other reasons cannot be opened for reading
+     */
     public static Integer[] search(String p, String filename) throws IOException {
+        File file = new File(filename);
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+        if (p.equals("") || file.length() == 0) {
+            Integer[] result = new Integer[] {};
+            return result;
+        }
         BufferedReader reader = new BufferedReader(new FileReader(filename));
         String line;
         List<Integer> result = new ArrayList<>();
-        int[] prefixFunc = new int[p.length()];
-        Arrays.fill(prefixFunc, 0);
-        int prefixFuncPrev = 0;
         int quantSymb = 0;
         while ((line = reader.readLine()) != null) {
-            System.out.println(line);
             String perfixLine = p + '#' + line;
+            int[] prefixFunc = new int[p.length()];
+            Arrays.fill(prefixFunc, 0);
+            int prefixFuncPrev = 0;
             for (int i = 1; i < perfixLine.length(); i++) {
                 int k = prefixFuncPrev;
                 while (k > 0 && perfixLine.charAt(k) != perfixLine.charAt(i)) {
@@ -40,12 +58,5 @@ public class SubStringSearch {
         Integer[] arr = new Integer[0];
         arr = result.toArray(arr);
         return arr;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        String searchingStr = "a";
-        Integer[] result = search(searchingStr, root + "src/main/java/ru/nsu/fit/ezaitseva/input.txt");
-        System.out.println(Arrays.toString(result));
     }
 }
--- Task_1_3_1/src/test/java/ru/nsu/fit/ezaitseva/TestSubStringSearch.java
+++ Task_1_3_1/src/test/java/ru/nsu/fit/ezaitseva/TestSubStringSearch.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 
@@ -27,19 +24,30 @@ public class TestSubStringSearch {
                 Arguments.of("a", "src/test/java/ru/nsu/fit/ezaitseva/test1.txt",
                         new Integer[] {0, 3, 8}),
                 Arguments.of("a", "src/test/java/ru/nsu/fit/ezaitseva/test2.txt",
-                        new Integer[] {0, 4, 5, 6, 7})
+                        new Integer[] {0, 4, 5, 6, 7}),
 
-//                Arguments.of("b", "src/test/java/ru/nsu/fit/ezaitseva/test3.txt", new Integer[] {3, 4}),
-//                Arguments.of("b", "src/test/java/ru/nsu/fit/ezaitseva/test4.txt", new Integer[] {3, 4})
-//
-//                Arguments.of("rrrrr", "test5.txt", new Integer[] {7}),
-//                Arguments.of("rrrrr", "test6.txt", new Integer[] {7})
+                Arguments.of("b", "src/test/java/ru/nsu/fit/ezaitseva/test3.txt", new Integer[] {3, 4}),
+                Arguments.of("b", "src/test/java/ru/nsu/fit/ezaitseva/test4.txt", new Integer[] {3, 4}),
 
-//                Arguments.of("ff", "TestFiles/test7.txt", []),
-//                Arguments.of("ff", "TestFiles/test8.txt", [13]),
-//
-//                Arguments.of("", "TestFiles/test9.txt", [])
-//                Arguments.of("", "TestFiles/test10.txt", [])
+                Arguments.of("rrrr", "src/test/java/ru/nsu/fit/ezaitseva/test5.txt", new Integer[] {7}),
+                Arguments.of("rrrr", "src/test/java/ru/nsu/fit/ezaitseva/test6.txt", new Integer[] {7}),
+
+                Arguments.of("ff", "src/test/java/ru/nsu/fit/ezaitseva/test7.txt", new Integer[0]),
+                Arguments.of("ff", "src/test/java/ru/nsu/fit/ezaitseva/test8.txt", new Integer[0]),
+
+                Arguments.of("", "src/test/java/ru/nsu/fit/ezaitseva/test9.txt", new Integer[0]),
+                Arguments.of("", "src/test/java/ru/nsu/fit/ezaitseva/test10.txt", new Integer[0]),
+
+                Arguments.of("er", "src/test/java/ru/nsu/fit/ezaitseva/test11.txt", new Integer[0]),
+                Arguments.of("n", "src/test/java/ru/nsu/fit/ezaitseva/test11.txt", new Integer[0]),
+
+                Arguments.of("it", "src/test/java/ru/nsu/fit/ezaitseva/test12.txt", new Integer[] {19, 54,
+                        413, 592, 612, 822, 959, 1018, 1050, 1115, 1182, 1257, 1288, 1352, 1568, 1762, 2067, 2115,
+                        2193, 2353, 2373, 2417, 2517, 2589, 2709, 2724, 2818, 2914, 3037, 3055, 3076, 3097, 3139, 3165,
+                        3272, 3512, 3837, 4019, 4048, 4068, 4093, 4163, 4301, 4407, 4457, 4483, 4670, 5057, 5153, 5351,
+                        5422, 5511, 5580, 5768, 5816, 5891, 6102, 6259, 6270, 6539, 6618, 6661, 6690}),
+
+                Arguments.of("", "src/test/java/ru/nsu/fit/ezaitseva/test12.txt", new Integer[0])
         );
     }
 }
