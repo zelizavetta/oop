7b8effb8bc0bf8fe98fa32d31781a7605b795a29
zelizavetta <zelizavetta@users.noreply.github.com>
2023-12-15 04:36:19 +0000

    Deploying to gh-pages from @ zelizavetta/oop@22a6d730967d546af4cdd1699679bbe6a5823106 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"nsu.fit.ezaitseva","l":"Tree.BfsIterator"},{"p":"nsu.fit.ezaitseva","l":"Tree"},{"p":"nsu.fit.ezaitseva","l":"TreeNode"}]
