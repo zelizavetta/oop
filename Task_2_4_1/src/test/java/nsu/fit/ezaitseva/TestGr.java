1e71c141cf8a19ac93ce4a48936528a367da3365
zelizavetta <e.zaitseva2@g.nsu.ru>
2024-06-02 20:19:05 +0700

    add test

---
+++ Task_2_4_1/src/test/java/nsu/fit/ezaitseva/TestGr.java
@@ -0,0 +1,14 @@
+package nsu.fit.ezaitseva;
+
+import nsu.fit.ezaitseva.model.entity.group.GroupConfig;
+import org.junit.jupiter.api.Test;
+
+public class TestGr {
+    @Test
+    void useless() {
+        GroupConfig groupConfig = new GroupConfig();
+        Main main = new Main();
+
+
+    }
+}
